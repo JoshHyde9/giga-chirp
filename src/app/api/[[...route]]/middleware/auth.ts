451c4ce59b8781ec250a10be4041e7facc68a563
JoshHyde9 <joshhyde546@gmail.com>
2024-09-04 18:29:20 +1000

    fix: plugin overwriting unauthed routes

--- src/app/api/[[...route]]/middleware/auth.ts
+++ src/app/api/[[...route]]/middleware/auth.ts
@@ -1,13 +1,11 @@
 import { auth } from "@/auth";
 import { Elysia } from "elysia";
 
-export const useAuth = new Elysia().derive(
-  { as: "global" },
-  async ({ error }) => {
+export const useAuth = (app: Elysia) =>
+  app.derive(async ({ error }) => {
     const session = await auth();
 
     if (!session) return error("Unauthorized", "Unauthorised");
 
     return { session };
-  }
-);
+  });
