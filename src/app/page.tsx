4f3e115121080989f50b98e57547c5b6d4c976a8
JoshHyde9 <joshhyde546@gmail.com>
2024-09-04 19:16:47 +1000

    feat: display posts on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
+import type { PostWithAuthor } from "@/lib/types";
+
 import { auth } from "@/auth";
+import { api } from "@/server/treaty";
+
+import { CreatePost } from "@/components/post/create-post";
+import { PostCard } from "@/components/post/post-card";
 import { SignOut } from "@/components/signout-button";
 
 export default async function Home() {
   const session = await auth();
 
+  const { data: posts } = await api.posts.all.get();
+
   return (
     <div>
       <div className="text-center">
-        <h1 className="font-bold text-3xl mb-4">Hello World!</h1>
+        <h1 className="font-bold text-3xl mb-4">Giga Chirp</h1>
       </div>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
 
       {session && <SignOut />}
+
+      <div className="mx-auto flex flex-col justify-center w-full lg:w-1/2">
+        {session && <CreatePost image_url={session.user.image_url} />}
+
+       {posts && posts.map((post: PostWithAuthor) => (
+        <PostCard key={post.id} post={post} />
+       ))}
+      </div>
     </div>
   );
 }
