73888bcbf0c262e585cc11604c91dad9736bc83b
JoshHyde9 <joshhyde546@gmail.com>
2024-09-06 11:10:17 +1000

    feat/fix: added routing and fixed styling

--- src/components/post/post-card.tsx
+++ src/components/post/post-card.tsx
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { Heart, Link, Mail, MessageCircle, Share } from "lucide-react";
+import { default as NextLink } from "next/link";
 
 import { PostWithAuthor } from "@/lib/types";
 
@@ -23,60 +24,66 @@ dayjs.extend(relativeTime);
 
 type PostCardProps = {
   post: PostWithAuthor;
+  className: string;
 };
 
 export const PostCard: React.FC<PostCardProps> = ({
-  post: { content, createdAt, author, _count },
+  post: { id, content, createdAt, author, _count },
+  className
 }) => {
   return (
-    // TODO: Click and route to post /[author]/status/[postId]
-    <Card className="flex w-full p-2 border-x-0 border-t shadow-none rounded-none last-of-type:border-b">
-      <div className="w-10 h-10 relative mt-5">
-        <Avatar>
-          <AvatarImage src={author.imageUrl} />
-          <AvatarFallback>{author.name[0]}</AvatarFallback>
-        </Avatar>
-      </div>
-      <div className="w-full">
-        <CardHeader className="flex flex-row items-center gap-x-2 pl-2 space-y-0">
-          <CardTitle className="text-xl">{author.name}</CardTitle>
-          <CardDescription>
-            @{author.username} <span>·</span> {dayjs(createdAt).fromNow()}
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="pl-2">
-          <p>{content}</p>
-        </CardContent>
-        <CardFooter className="flex gap-x-4 justify-between py-1">
-          <div className="flex items-center text-sm gap-x-1 duration-300 hover:text-blue-500 hover:cursor-pointer">
-            <MessageCircle className="size-4" />
-            <span>{_count.replies}</span>
-          </div>
-          <div className="flex items-center text-sm gap-x-1 duration-300 hover:text-red-500 hover:cursor-pointer">
-            <Heart className="size-4" />
-            <span>{_count.likes}</span>
-          </div>
-          <div>
-            <Popover>
-              <PopoverTrigger className="py-2">
-                <Share className="size-4 duration-300 hover:text-blue-500 hover:cursor-pointer" />
-              </PopoverTrigger>
-              <PopoverContent className="cursor-pointer">
-                {/* TODO: Copy post to clipboard */}
-                <div className="flex items-center gap-x-1">
-                  <Link className="size-5" />
-                  <span className="font-semibold">Copy link</span>
-                </div>
+    // TODO: Make footer buttons not route to page (also turn into actual buttons)
+    <NextLink href={`/${author.username}/status/${id}`} className={className}>
+      <Card className="flex w-full p-2 shadow-none rounded-none">
+        <div className="w-10 h-10 relative mt-5">
+          <Avatar>
+            <AvatarImage src={author.imageUrl} />
+            <AvatarFallback>{author.name[0]}</AvatarFallback>
+          </Avatar>
+        </div>
+        <div className="w-full">
+          <CardHeader className="flex flex-row items-center gap-x-2 pl-2 space-y-0">
+            <CardTitle className="text-xl">{author.name}</CardTitle>
+            <CardDescription>
+              @{author.username} <span>·</span> {dayjs(createdAt).fromNow()}
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="pl-2">
+            <p>{content}</p>
+          </CardContent>
+          <CardFooter className="flex gap-x-4 justify-between py-1">
+            <div className="flex items-center text-sm gap-x-1 duration-300 hover:text-blue-500 hover:cursor-pointer">
+              <MessageCircle className="size-4" />
+              <span>{_count.replies}</span>
+            </div>
+            <div className="flex items-center text-sm gap-x-1 duration-300 hover:text-red-500 hover:cursor-pointer">
+              <Heart className="size-4" />
+              <span>{_count.likes}</span>
+            </div>
+            <div>
+              <Popover>
+                <PopoverTrigger className="py-2">
+                  <Share className="size-4 duration-300 hover:text-blue-500 hover:cursor-pointer" />
+                </PopoverTrigger>
+                <PopoverContent className="cursor-pointer">
+                  {/* TODO: Copy post to clipboard */}
+                  <div className="flex items-center gap-x-1">
+                    <Link className="size-5" />
+                    <span className="font-semibold">Copy link</span>
+                  </div>
 
-                <div className="flex items-center gap-x-1 mt-2">
-                  <Mail className="size-5" />
-                  <span className="font-semibold">Send via Direct Message</span>
-                </div>
-              </PopoverContent>
-            </Popover>
-          </div>
-        </CardFooter>
-      </div>
-    </Card>
+                  <div className="flex items-center gap-x-1 mt-2">
+                    <Mail className="size-5" />
+                    <span className="font-semibold">
+                      Send via Direct Message
+                    </span>
+                  </div>
+                </PopoverContent>
+              </Popover>
+            </div>
+          </CardFooter>
+        </div>
+      </Card>
+    </NextLink>
   );
 };
